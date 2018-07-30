894db2ab2bee0f1972972ec52866dbb76a459e08
lawyso <40557870+lawyso@users.noreply.github.com>
2018-07-30 19:01:51 +0300

    Update settings.py

--- Website/settings.py
+++ Website/settings.py
@@ -219,3 +219,7 @@ TINYMCE_DEFAULT_CONFIG = {
     'menubar': True,
     'statusbar': True,
     }
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
